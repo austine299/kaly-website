7a29bb6df523adfeaf58ab3f3cc737022992fac4
Obiora Imah <obiora.imah@gmail.com>
2025-06-03 06:14:22 -0400

    add vercel and meta tags

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
+import {Helmet} from "react-helmet";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Product from './components/Product';
-// import About from './components/About';
-// import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Kaly's Grain n More</title>
+        <meta name="description" content="Welcome to Kaly's Grain n More. Your one-stop shop for all things grocery and more!" />
+      </Helmet>
       <Router>
         <Routes>
           <Route path='/' element ={<Home/>}/>

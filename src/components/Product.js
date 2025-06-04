4637e5730534332143c459a694ad0387ddde3d20
imah ugochukwu <ugochukwuaugustine299@gmail.com>
2025-06-04 16:52:48 +1100

    updated commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,30 +16,30 @@ function Product({ productRef }) {
     <div ref={productRef} className="flex flex-col gap-6 items-center">
       <h1 className="text-2xl font-bold p-7">Our Product</h1>
       <div className="flex flex-col gap-6 items-center py-16 px-6 md:px-20 w-full bg-red-50 ">
-        <div className="grid sm:grid-cols-5 grid-cols-3 justify-center gap-9 sm:max-w-6xl h-full mx-auto">
+        <div className="flex sm:flex-row flex-cols flex-wrap  gap-9 sm:max-w-6xl h-full mx-auto">
           {products.map((product) => (
             <div
               onClick={() => handleProductClick(product)}
               key={product.id}
               className={
                 selectedProduct?.id === product.id
-                  ? "flex flex-col justify-center items-center text-center  w-full sm:w-full h-full mx-auto  relative cursor-pointer"
+                  ? "flex justify-center items-center text-center  w-full sm:w-full h-full mx-auto  relative cursor-pointer"
                   : "flex flex-col justify-center items-center text-center  cursor-pointer"
               }
             >
               <div className={selectedProduct?.id === product.id
-                  ? "flex flex-col justify-center items-center text-center w-full sm:w-full h-full mx-auto my-auto bg-white rounded-md p-2"
+                  ? "flex flex-col justify-center items-center text-center w-10/12 sm:w-1/2 h-full mx-auto my-auto bg-white rounded-md p-4"
                   : ""}>
                 <img
                   src={require(`../images/${product.image}`)}
                   alt="product image1"
                   className={
                     selectedProduct?.id === product.id
-                      ? "sm:max-w-4xl w-1/2 h-1/2  rounded-md"
+                      ? "sm:w-72 w-72 h-60  rounded-md"
                       : "sm:w-48 sm:h-48 w-32 h-32  rounded-md"
                   }
                 />
-                <span className="font-bold">{product.name}</span>
+                <p className="font-bold sm:w-48 w-32">{product.name}</p>
                 {selectedProduct?.id === product.id && (
                   <span className="text-red-500 font-bold">only a few left to grab and we deliver to your door step</span>
                 )}

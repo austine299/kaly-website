4016aad5964f02c64bfbba2ea26c79270187bb20
imah ugochukwu <ugochukwuaugustine299@gmail.com>
2025-06-06 12:10:19 +1100

    update checkout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,8 @@ import { CartContext } from "./CartContext";
 import { Link } from "react-router-dom";
 
 function Cart() {
-  const { cartItems,increaseQty, decreaseQty, removeFromCart } = useContext(CartContext);
+  const { cartItems, increaseQty, decreaseQty, removeFromCart } =
+    useContext(CartContext);
 
   const totalPrice = cartItems.reduce(
     (sum, item) => sum + item.price * item.quantity,
@@ -34,9 +35,9 @@ function Cart() {
         {cartItems.map((item) => (
           <div
             key={item.id}
-            className="flex sm:flex-row flex-col items-center justify-between border p-4 rounded-lg shadow"
+            className="flex sm:flex-row flex-col sm:items-center gap-4 sm:justify-between border p-4 rounded-lg shadow"
           >
-            <div className="flex items-center gap-4">
+            <div className="flex justify-center items-center gap-4">
               <img
                 src={require(`../images/${item.image}`)}
                 alt={item.name}
@@ -46,23 +47,30 @@ function Cart() {
                 <h3 className="font-bold text-lg">{item.name}</h3>
                 <p className="font-semibold">Quantity: {item.quantity}</p>
               </div>
-            </div> 
-            <div className="flex items-center gap-7 border border-red-600 rounded-md w-fit">
+            </div>
+            <div className="flex justify-around items-start sm:items-center gap-7">
+              <div className="flex items-center gap-7 border border-red-600 rounded-md w-fit">
+                <button
+                  onClick={() => decreaseQty(item.id)}
+                  className="px-4 py-1 bg-red-600 hover:bg-red-400 text-3xl rounded-l-md text-white"
+                >
+                  -
+                </button>
+                <span className="py-1">{item.quantity}</span>
+                <button
+                  onClick={() => increaseQty(item.id)}
+                  className="px-4 py-1 bg-red-600 hover:bg-red-400 text-3xl rounded-r-md text-white"
+                >
+                  +
+                </button>
+              </div>
               <button
-                onClick={() => decreaseQty(item.id)}
-                className="px-4 py-1 bg-red-600 hover:bg-red-400 text-3xl rounded-l-md text-white"
+                onClick={() => removeFromCart(item.id)}
+                className="hover:bg-red-600 px-4 py-3 rounded-md font-bold hover:text-white"
               >
-                -
-              </button>
-              <span className="py-1">{item.quantity}</span>
-              <button
-                onClick={() => increaseQty(item.id)}
-                className="px-4 py-1 bg-red-600 hover:bg-red-400 text-3xl rounded-r-md text-white"
-              >
-                +
+                Del
               </button>
             </div>
-            <button onClick={() => removeFromCart(item.id)} className="hover:bg-red-600 px-3 py-1 rounded-md font-bold hover:text-white">Del</button>
             <div className="sm:text-xl font-bold">
               clicked on checkout to send your cart to my DM
             </div>
@@ -71,7 +79,6 @@ function Cart() {
       </div>
 
       <div className="mt-6 text-right">
-       
         <Link to="/checkout">
           <button className="mt-4 px-6 py-2 bg-red-600 text-white font-bold rounded hover:bg-red-500">
             Proceed to Checkout
